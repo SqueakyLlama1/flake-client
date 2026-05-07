db19a3b8107c686d6ae7deba7dd435ee5e6bd1be
Christian Bone <bone.christian.c@outlook.com>
2026-05-07 14:12:56 -0600

    Add skin stuff, start on UI

--- launcher/modules/index.js
+++ launcher/modules/index.js
@@ -1,3 +1,15 @@
 import * as load from './load.js';
+import * as skins from './skin_grabber.js';
 
 load.init();
+async function testSkin() {
+    const skinURL = await skins.get('ashtonbashton78').then(result => {
+        if (result.error) {
+            console.error("Error:", result.error);
+            return;
+        }
+        return { url: result.skinUrl, uuid: result.uuid };
+    });
+    skins.assemble(skinURL.url, skinURL.uuid, true);
+}
+testSkin();
